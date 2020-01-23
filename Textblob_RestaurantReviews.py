52257721d829624f7034c6cd862e6bae25286a9c
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-01-23 15:14:24 +0100

    Analysed file for IMDB Review Textblobs changed

--- Textblob_RestaurantReviews.py
+++ Textblob_RestaurantReviews.py
@@ -11,16 +11,16 @@ def sentiment_calc(Review):
     except:
         return None
 
-df['Sentiment'] = df['Review'].apply(sentiment_calc)
+df['Result'] = df['Review'].apply(sentiment_calc)
 
-# Splits the Sentiment Column into Polarity and Subjectivity
-sentiment_series = df['Sentiment'].tolist()
+# Splits the Result Column into Polarity and Subjectivity
+sentiment_series = df['Result'].tolist()
 columns = ['Polarity', 'Subjectivity']
 df2 = pd.DataFrame(sentiment_series, columns=columns, index=df.index)
 
 # Combine both DataFrames into 1
 df3 = pd.merge(df, df2, left_index=True, right_index=True)
-# deletes the column Sentiment, so that there's no redundant data
-del df3['Sentiment']
+# deletes the column Result, so that there's no redundant data
+del df3['Result']
 # save the new Dataset into a csv file
 df3.to_csv('Analysed_Restaurant_Review.csv', index=False)
