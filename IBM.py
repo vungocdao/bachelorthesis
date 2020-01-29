844e74b23c1e7f0bf2150df107d5568393359704
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-01-30 00:23:39 +0100

    approach for IBM and StanfordCoreNLP

---
+++ IBM.py
@@ -0,0 +1,20 @@
+import json
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+authenticator = IAMAuthenticator("wWb3QxJovOh6mfXU_a-vjLo4QpYQnvJpfDBbVEhiCWqv")
+tone_analyzer = ToneAnalyzerV3 (
+    version = '2017-09-21',
+    authenticator=authenticator
+)
+
+tone_analyzer.set_service_url('https://api.eu-de.tone-analyzer.watson.cloud.ibm.com/instances/d967bd97-be2d-4b92-a8b4-79c1c54ae9a2')
+
+text = 'Yey, he is such a nice guy'
+
+tone_analysis = tone_analyzer.tone(
+    {'text': text},
+    content_type='application/json').get_result()
+
+print(json.dumps(tone_analysis, indent=2))
