7fc7037b653f6df411c6a3d62f45dcc4c4508033
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-01-29 22:02:31 +0100

    nothing worked yey

--- Deepaffects.py
+++ Deepaffects.py
@@ -1,48 +1,20 @@
-from __future__ import prin_statement
 import deepaffects
 from deepaffects.rest import ApiException
 from pprint import pprint
 import pandas as pd
-
-# Initializes a DataFrame out of the csv file
-df = pd.read_csv("xxx.csv")
+import json
 
 # API key to access DeepAffects API
-deepaffects.configuration.api_key['apikey'] = '<dKsguddWi6LYRexyd6SOndI3vKrAyhgU>'
+deepaffects.configuration.api_key['apikey'] = '5O5rfOOcxmKFfBcqNZJQdRAMWOz6K8Lz'
 
 # Create API Class with sentence that needs to be featurized
+data = {
+        "content": "Zaphod Beeblebrox is a nice guy"}
 api_instance = deepaffects.EmotionApi()
-var body = {
-    "content": "TEXT"
-}
+
 
 try:
-    api_response = api_instance.sync_text_recognise_emotion(body)
+    api_response = api_instance.text_recognise_emotion(data)
     pprint(api_response)
-except ApiExcpetion as e:
-    print("Exception when calling EmotionApi->sync_recognise_emotion: %\n" % e)
-
-"""
-
-# Iterates through the Column and applies Textblob.sentiment
-def sentiment_calc(columnname):
-    try:
-        return TextBlob(columnname).sentiment
-    except:
-        return None
-
-df['Result'] = df['columnname'].apply(sentiment_calc)
-
-# Splits the Result Column into Polarity and Subjectivity
-sentiment_series = df['Result'].tolist()
-columns = ['Polarity', 'Subjectivity']
-df2 = pd.DataFrame(sentiment_series, columns=columns, index=df.index)
-
-# Combine both DataFrames into 1
-df3 = pd.merge(df, df2, left_index=True, right_index=True)
-# deletes the column Result, so that there's no redundant data
-del df3['Result']
-# save the new Dataset into a csv file
-df3.to_csv('Textblob_Analysed_Domain_Review.csv', index=False)
-
-"""
+except ApiException as e:
+    print("Exception when calling EmotionApi->sync_recognise_emotion: %s\n" % e)
