432f86b9cf523e795be8adc1d19a81967dab2c99
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-02-27 23:06:24 +0100

    lowercase, beginning of removeStopWords

---
+++ lowercase.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+ds1 = "Amazon_Reviews.csv"
+ds2 = "Amazon_Reviews_sample.csv"
+ds3 = "IMDB_Reviews.csv"
+ds4 = "IMDB_Reviews_sample.csv"
+ds5 = "Restaurant_Reviews.csv"
+ds6 = "Restaurant_Reviews_sample.csv"
+ds7 = "Uber_Ride_Reviews.csv"
+ds8 = "Uber_Ride_Reviews_sample.csv"
+ds9 = "Yelp_Reviews.csv"
+ds10 = "Yelp_Reviews_sample.csv"
+
+def lowercase(File):
+    df = pd.read_csv(File)
+    df['Review'] = df['Review'].str.lower()
+    df.to_csv(File)
+
+lowercase(ds1)
+lowercase(ds2)
+lowercase(ds3)
+lowercase(ds4)
+lowercase(ds5)
+lowercase(ds6)
+lowercase(ds7)
+lowercase(ds8)
+lowercase(ds9)
+lowercase(ds10)
---
+++ removeStopWords.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+ds1 = "Amazon_Reviews.csv"
+ds2 = "Amazon_Reviews_sample.csv"
+ds3 = "IMDB_Reviews.csv"
+ds4 = "IMDB_Reviews_sample.csv"
+ds5 = "Restaurant_Reviews.csv"
+ds6 = "Restaurant_Reviews_sample.csv"
+ds7 = "Uber_Ride_Reviews.csv"
+ds8 = "Uber_Ride_Reviews_sample.csv"
+ds9 = "Yelp_Reviews.csv"
+ds10 = "Yelp_Reviews_sample.csv"
+
+def removeStopWords(File):
+    df = pd.read_csv(File)
+    df['Review'] = df['Review'].replace()
+    df.to_csv(File)
+
+removeStopWords(ds1)
+removeStopWords(ds2)
+removeStopWords(ds3)
+removeStopWords(ds4)
+removeStopWords(ds5)
+removeStopWords(ds6)
+removeStopWords(ds7)
+removeStopWords(ds8)
+removeStopWords(ds9)
+removeStopWords(ds10)
