96da35f02b96afe918aaa1cf23b97d044ffe85a0
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-03-12 00:17:22 +0100

    folder structure

---
+++ Useful Scripts/ConvertToFiletype/ReviewToTxt.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+ds1 = "Amazon_Reviews"
+ds2 = "IMDB_Reviews"
+ds3 = "Restaurant_Reviews"
+ds4 = "Uber_Ride_Reviews"
+ds5 = "Yelp_Reviews"
+
+def ReviewToTxt(file):
+    read_file = pd.read_csv(file + ".csv")
+    read_file['Review_new'] = read_file['Review'].astype(str) + '.'
+    read_file2 = read_file['Review_new']
+    read_file2.to_csv(file + ".txt", sep='\n', index = None)
+
+ReviewToTxt(ds1)
+ReviewToTxt(ds2)
+ReviewToTxt(ds3)
+ReviewToTxt(ds4)
+ReviewToTxt(ds5)
