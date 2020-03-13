72e1c84fe13b480192a9584326e8cd08bceaf430
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-03-13 11:49:41 +0100

    print line

--- Datasets/Analysed Dataset (.csv, no calc)/Stanford/PrintPolarity.py
+++ Datasets/Analysed Dataset (.csv, no calc)/Stanford/PrintPolarity.py
@@ -4,5 +4,4 @@ with open('Result_Restaurant.txt', 'r') as f:
     for line in f:
         count+=1
         if count % 2 != 0:
-            with open ('Result_Restaurant_Polarity.txt', 'a') as f:
-                f.write(line)
+            print(line)
