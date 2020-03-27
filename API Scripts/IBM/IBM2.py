0ba48559a30c568de1797c1a83462b3c296497cc
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-03-27 13:56:40 +0100

    IBM attempts added.

--- API Scripts/IBM/IBM2.py
+++ API Scripts/IBM/IBM2.py
@@ -1,7 +1,7 @@
 import json
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
+import pandas as pd
 
 authenticator = IAMAuthenticator("wWb3QxJovOh6mfXU_a-vjLo4QpYQnvJpfDBbVEhiCWqv")
 tone_analyzer = ToneAnalyzerV3 (
@@ -11,10 +11,16 @@ tone_analyzer = ToneAnalyzerV3 (
 
 tone_analyzer.set_service_url('https://api.eu-de.tone-analyzer.watson.cloud.ibm.com/instances/d967bd97-be2d-4b92-a8b4-79c1c54ae9a2')
 
-text = 'Yey, he is such a nice guy'
 
-tone_analysis = tone_analyzer.tone(
-    {'text': text},
-    content_type='application/json').get_result()
+df = pd.read_csv("Amazon_Reviews.csv")
+df2 = df['Review'].head(5)
+list = df2.tolist()
 
-print(json.dumps(tone_analysis, indent=2))
+text = list
+
+for i in range (len(list)):
+    text = list[i]
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        content_type='application/json').get_result()
+    print(json.dumps(tone_analysis, indent=2))
---
+++ API Scripts/IBM/test3.py
@@ -0,0 +1,33 @@
+{
+  "document_tone": {
+    "tones": [
+      {
+        "score": 0.790892,
+        "tone_id": "joy",
+        "tone_name": "Joy"
+      }
+    ]
+  }
+}
+{
+  "document_tone": {
+    "tones": [
+      {
+        "score": 0.790892,
+        "tone_id": "joy",
+        "tone_name": "Joy"
+      }
+    ]
+  }
+}
+{
+  "document_tone": {
+    "tones": [
+      {
+        "score": 0.790892,
+        "tone_id": "joy",
+        "tone_name": "Joy"
+      }
+    ]
+  }
+}
