374c02ccf2359e6f8bd916d93205dcc82c3603ae
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-04-02 21:29:36 +0200

    Differentiation between NaiveBayes and Pattern Textblob

---
+++ API Scripts/TextBlob/textblobApply.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from textblob import TextBlob
+
+# Variables with name of csv file
+ds1 = "Amazon_Reviews.csv"
+ds2 = "Amazon_Reviews_sample.csv"
+ds3 = "IMDB_Reviews.csv"
+ds4 = "IMDB_Reviews_sample.csv"
+ds5 = "Restaurant_Reviews.csv"
+ds6 = "Restaurant_Reviews_sample.csv"
+ds7 = "Uber_Ride_Reviews.csv"
+ds8 = "Uber_Ride_Reviews_sample.csv"
+ds9 = "Yelp_Reviews.csv"
+ds10 = "Yelp_Reviews_sample.csv"
+
+# Iterates through the Column and applies textblob.sentiment
+def sentiment_calc(Review):
+    try:
+        return TextBlob(Review).sentiment
+    except:
+        return None
+
+# Initializes DataFrame, Applies API and creates 'Result' column, saves into new File
+def apply_analysis(ds):
+    df = pd.read_csv(ds)
+    df['Result'] = df['Review'].apply(sentiment_calc)
+    # Splits the Result Column into Polarity and Subjectivity
+    sentiment_series = df['Result'].tolist()
+    columns = ['Polarity_result', 'Subjectivity_result']
+    df2 = pd.DataFrame(sentiment_series, columns=columns, index=df.index)
+    # Combine both DataFrames into 1
+    df3 = pd.merge(df, df2, left_index=True, right_index=True)
+    # deletes the column Result, so that there's no redundant data
+    del df3['Result']
+    # save the new Dataset into a csv file
+    df3.to_csv('Textblob_Analysed_' + ds, index=False)
+
+apply_analysis(ds1)
+# apply_analysis(ds2)
+apply_analysis(ds3)
+# apply_analysis(ds4)
+apply_analysis(ds5)
+# apply_analysis(ds6)
+apply_analysis(ds7)
+# apply_analysis(ds8)
+apply_analysis(ds9)
+# apply_analysis(ds10)
--- API Scripts/TextBlob/textblobApplyRefined.py
+++ API Scripts/TextBlob/textblobApplyRefined.py
@@ -16,32 +16,23 @@ ds10 = "Yelp_Reviews_sample.csv"
 # Iterates through the Column and applies textblob.sentiment
 def sentiment_calc(Review):
     try:
-        return TextBlob(Review).sentiment
+        return TextBlob(Review).sentiment.polarity
     except:
         return None
 
 # Initializes DataFrame, Applies API and creates 'Result' column, saves into new File
 def apply_analysis(ds):
     df = pd.read_csv(ds)
-    df['Result'] = df['Review'].apply(sentiment_calc)
-    # Splits the Result Column into Polarity and Subjectivity
-    sentiment_series = df['Result'].tolist()
-    columns = ['Polarity_result', 'Subjectivity_result']
-    df2 = pd.DataFrame(sentiment_series, columns=columns, index=df.index)
-    # Combine both DataFrames into 1
-    df3 = pd.merge(df, df2, left_index=True, right_index=True)
-    # deletes the column Result, so that there's no redundant data
-    del df3['Result']
-    # save the new Dataset into a csv file
-    df3.to_csv('Textblob_Analysed_' + ds, index=False)
+    df['Polarity_result'] = df['Review'].apply(sentiment_calc)
+    df.to_csv('Textblob_Analysed_PA_' + ds, index=False)
 
 apply_analysis(ds1)
-apply_analysis(ds2)
+# apply_analysis(ds2)
 apply_analysis(ds3)
-apply_analysis(ds4)
+# apply_analysis(ds4)
 apply_analysis(ds5)
-apply_analysis(ds6)
+# apply_analysis(ds6)
 apply_analysis(ds7)
-apply_analysis(ds8)
+# apply_analysis(ds8)
 apply_analysis(ds9)
-apply_analysis(ds10)
+# apply_analysis(ds10)
