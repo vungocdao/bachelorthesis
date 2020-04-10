db2ce735c7a8a69b8a5cd7b26bab928982322253
Quang Dao Vu Ngoc <quang.vu-ngoc@stud.uni-due.de>
2020-04-10 14:13:03 +0200

    .

--- API Scripts/Repustate/scraping.py
+++ API Scripts/Repustate/scraping.py
@@ -1,11 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs4
 
 r = requests.get('https://www.repustate.com/sentiment-analysis-api-demo/')
 
-# soup = bs4.BeautifulSoup(request.text, 'lxml')
-# sentiment = soup.select('strong')
-
+soup = bs4.BeautifulSoup(request.text, 'lxml')
+sentiment = soup.select('strong')
+print(soup)
+print(sentiment)
 # <textarea id="id_text" class="ctrl" name="text"> Insert Reviews into this
 # <button class="btn btn--primary"> button
 # in <td>, <strong> sentiment value
